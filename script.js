c17054ec343ccedb1edcf1a11b417ecad0065d54
abdelrahmanashraf11coding <abdelrahmanashraf11coding>
2024-06-25 21:39:23 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer',  'Graphic Design', 'Web Designer','UI/UX Design','YouTuber'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 1200,
+    loop: true,
+  });
