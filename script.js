e9279db1fda24cd73edb9b82a4d7ba623019911e
abdelrahmanashraf11coding <abdelrahmanashraf11coding>
2024-07-25 14:53:44 +0300

    apdat

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-
+// let menu = document.querySelector('#menu-icon');
+// let navbar = document.querySelector('.navbar');
+// menu.onclick = () => {
+//   menu.classList.toggle('bx-x');
+//   navbar.classList.toggle('active');
+// }
+// window.onscroll = () => {
+//   menu.classList.remove('bx-x');
+//   navbar.classList.remove('active');
+// } 
 var typed = new Typed('.multiple-text', {
     strings: ['Frontend Developer',  'Graphic Design', 'Web Designer','UI/UX Design','YouTuber'],
     typeSpeed: 80,
