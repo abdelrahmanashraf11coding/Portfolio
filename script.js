2e5ce5bb79172ca5f517c313f7031b3624a2e4d0
abdelrahmanashraf11coding <abdelrahmanashraf11coding>
2024-07-26 12:07:36 +0300

    updat

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// let menu = document.querySelector('#menu-icon');
-// let navbar = document.querySelector('.navbar');
-// menu.onclick = () => {
-//   menu.classList.toggle('bx-x');
-//   navbar.classList.toggle('active');
-// }
-// window.onscroll = () => {
-//   menu.classList.remove('bx-x');
-//   navbar.classList.remove('active');
-// } 
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+  var toggleButton = document.querySelector(".toggle-btn");
+  if (document.body.classList.contains("dark-mode")) {
+      toggleButton.textContent = "Light Mode";
+  } else {
+      toggleButton.textContent = "Dark Mode";
+  }
+}
+
 var typed = new Typed('.multiple-text', {
     strings: ['Frontend Developer',  'Graphic Design', 'Web Designer','UI/UX Design','YouTuber'],
     typeSpeed: 80,
